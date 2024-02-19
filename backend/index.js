805234f8db4406782d21d107bd3346cf7eac0a4a
rahmanasad222 <asadur.rehman@cheetay.pk>
2024-02-19 19:34:58 +0500

    vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ const resumeInfoRoute = require("./routes/resumeInfoRoute");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: '',
+    methods: ['POST', 'GET'],
+    credentials:'true'
+  }
+));
 app.use(express.json());
 
 // initializing DB connection
