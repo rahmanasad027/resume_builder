aeac5282e7c569382625accfa2f2d99909cd1ce7
rahmanasad027 <78791832+rahmanasad027@users.noreply.github.com>
2024-02-19 20:02:55 +0500

    Update api-caller.js

--- frontend/src/api/api-caller.js
+++ frontend/src/api/api-caller.js
@@ -1,6 +1,8 @@
 import Axios from 'axios';
 import { BASE_URL } from './constants';
+import axios from 'axios';
 //  api caller function to manage connections in RESTFUL manner. this is the last stage of our frontend application from where our data leaves our frontend application
+axios.defaults.withCredentials = true;
 export function apiCaller({ method = 'GET', url = '',  data = {}, contentType = 'application/json' } = {}) {
   return Axios({
     method,
