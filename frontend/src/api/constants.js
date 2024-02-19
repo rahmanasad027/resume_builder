f49d1867acf1be41cf6d9ab05007c746e1516e3d
rahmanasad027 <78791832+rahmanasad027@users.noreply.github.com>
2024-02-19 20:00:22 +0500

    Update constants.js

--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -1,5 +1,5 @@
 //  Setting up all the constants like baseUrl and endpoints to be used.
-export const BASE_URL = 'http://localhost:4000';
+export const BASE_URL = 'https://resume-builder1-dun.vercel.app/';
 export const ENDPOINTS = {
   PERSONAL_INFO: `${BASE_URL}/data`,
   PROFESSIONAL_INFO: `${BASE_URL}/professional-data`,
