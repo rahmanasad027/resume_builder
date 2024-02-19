352261ff2cfa5083efbd3074b71d1af3dec18ee7
rahmanasad027 <78791832+rahmanasad027@users.noreply.github.com>
2024-02-19 20:09:25 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors(
   {
-    origin: 'https://resume-builder-1wqd.vercel.app/',
+    origin: 'https://resume-builder-frontend-seven.vercel.app/',
     methods: ['POST', 'GET'],
     credentials:'true'
   }
